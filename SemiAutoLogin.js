ed922d1bdb8435f1343782ca87b54b4a78dad10a
Murturtle <97775022+Murturtle@users.noreply.github.com>
2024-03-10 22:11:45 -0700

    Update SemiAutoLogin.js

--- SemiAutoLogin.js
+++ SemiAutoLogin.js
@@ -1,15 +1,15 @@
 (() => {
-  const popupCenter = ({url, title, w, h}) => {
-      const dualScreenLeft = window.screenLeft !==  undefined ? window.screenLeft : window.screenX;
-      const dualScreenTop = window.screenTop !==  undefined   ? window.screenTop  : window.screenY;
+  var popupCenter = ({url, title, w, h}) => {
+      var dualScreenLeft = window.screenLeft !==  undefined ? window.screenLeft : window.screenX;
+      var dualScreenTop = window.screenTop !==  undefined   ? window.screenTop  : window.screenY;
   
-      const width = window.innerWidth ? window.innerWidth : document.documentElement.clientWidth ? document.documentElement.clientWidth : screen.width;
-      const height = window.innerHeight ? window.innerHeight : document.documentElement.clientHeight ? document.documentElement.clientHeight : screen.height;
+      var width = window.innerWidth ? window.innerWidth : document.documentElement.clientWidth ? document.documentElement.clientWidth : screen.width;
+      var height = window.innerHeight ? window.innerHeight : document.documentElement.clientHeight ? document.documentElement.clientHeight : screen.height;
   
-      const systemZoom = width / window.screen.availWidth;
-      const left = (width - w) / 2 / systemZoom + dualScreenLeft
-      const top = (height - h) / 2 / systemZoom + dualScreenTop
-      const newWindow = window.open(url, title, 
+      var systemZoom = width / window.screen.availWidth;
+      var left = (width - w) / 2 / systemZoom + dualScreenLeft
+      var top = (height - h) / 2 / systemZoom + dualScreenTop
+      var newWindow = window.open(url, title, 
         `
         scrollbars=yes,
         width=${w / systemZoom}, 
@@ -22,18 +22,18 @@
       return newWindow;
   }
   ModAPI.require("player");
-  const w = 200;
-  const h = 200;
-  const newWin = popupCenter({url: '', title: '', w: 200, h: 100});
-  const pass = "";
+  var w = 200;
+  var h = 200;
+  var newWin = popupCenter({url: '', title: '', w: 200, h: 100});
+  var pass = "";
 
   if(!newWin || newWin.closed || typeof newWin.closed=='undefined') 
   { 
-      const pass = prompt("login (popup failed!)");
+      pass = prompt("login (popup failed!)");
   } else {
     newWin.document.body.innerHTML="<center><form id='form'><input id='pass' type='password' autofocus/><br><br><input type='submit' value='Register'></form></center>";
     newWin.document.getElementById("form").onsubmit=function(){
-        const pass = newWin.document.getElementById("pass").value;
+        pass = newWin.document.getElementById("pass").value;
         newWin.close();
     };
   }
