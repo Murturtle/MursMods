e7dc9970f44fda8faae753e5779f3cad562539de
Murturtle <97775022+Murturtle@users.noreply.github.com>
2024-03-10 20:41:50 -0700

    Update Blur.js

--- Blur.js
+++ Blur.js
@@ -1,19 +1,21 @@
-ModAPI.require("settings");
-var oldFPS = 260;
-function fcs(){
-    if(ModAPI.settings != null){
-        ModAPI.settings.limitFramerate = oldFPS;
-        ModAPI.settings.reload();
+(() => {
+    ModAPI.require("settings");
+    var oldFPS = 260;
+    function fcs(){
+        if(ModAPI.settings != null){
+            ModAPI.settings.limitFramerate = oldFPS;
+            ModAPI.settings.reload();
+        }
     }
-}
-
-function blr(){
-    oldFPS = ModAPI.settings.limitFramerate;
-    if(ModAPI.settings != null){
-        ModAPI.settings.limitFramerate = 5;
-        ModAPI.settings.reload();
+    
+    function blr(){
+        oldFPS = ModAPI.settings.limitFramerate;
+        if(ModAPI.settings != null){
+            ModAPI.settings.limitFramerate = 5;
+            ModAPI.settings.reload();
+        }
     }
-}
-
-addEventListener("focus",fcs);
-addEventListener("blur",blr);
+    
+    addEventListener("focus",fcs);
+    addEventListener("blur",blr);
+});
